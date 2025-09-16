ea9fd4079ae829d4f610a24228d172d74cffe8f6
Ritochit Ghosh <imrito18@gmail.com>
2025-09-16 21:24:46 +0530

    Fix: Added openapi.yaml into image

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -53,7 +53,9 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-const swaggerDocument = YAML.load("./openapi.yaml");
+const swaggerDocument = YAML.load(
+  path.join(__dirname, "../openapi.yaml")
+);
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
