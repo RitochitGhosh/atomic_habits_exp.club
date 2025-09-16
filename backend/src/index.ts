3c3aa7b6b9804eb70fbc15f68060f1cb3d9feab9
Ritochit Ghosh <imrito18@gmail.com>
2025-09-16 23:47:49 +0530

    Fix: Changes in dockerfile + index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -54,8 +54,9 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 const swaggerDocument = YAML.load(
-  path.join(__dirname, "openapi.yaml") // [!code ++]
+  path.join(__dirname, "../openapi.yaml")
 );
+
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Routes
